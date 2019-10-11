ba76b9d6374fdf977e12cd4d0d57092f9926e0c2
Maksthorn <maksdrzezdzon@gmail.com>
2019-10-11 23:09:35 +0100

    im dead, basics of flask down

--- flaskblog.py
+++ flaskblog.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "hello"
+	return "hello test"
+
+@app.route("/about")
+def about():
+	return "about page"
 
 if __name__ == '__main__':
+    # allows you to dynamically update site like in angular
     app.debug = True
+    # sets values
     app.run(host = '0.0.0.0', port = 5000)
