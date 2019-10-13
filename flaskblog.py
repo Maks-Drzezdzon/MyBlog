412a4af49f7a340e4ad86fb478c8f483c2a24378
Maksthorn <maksdrzezdzon@gmail.com>
2019-10-13 19:53:34 +0100

    adding some filler data

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,32 @@
 from flask import Flask
+from flask import render_template
+import os
 app = Flask(__name__)
 
 # https://www.youtube.com/watch?v=MwZwr5Tvyxo
+messages = [
+                {'author':'name',
+                 'title':'filler title',
+                 'content':'message',
+                 'date_posted':'12-12-10'},
+                
+                {'author':'name',
+                 'title':'filler title',
+                 'content':'message',
+                 'date_posted':'12-12-10'}
+            ]
+
 
 @app.route("/")
+@app.route("/home")
 def hello():
-	return "hello test"
+	return render_template('home.html')
 
 @app.route("/about")
 def about():
-	return "about page"
+	return render_template("about.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # allows you to dynamically update site like in angular
-    app.debug = True
     # sets values
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8080, debug = True)
