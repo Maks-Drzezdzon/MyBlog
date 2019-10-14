737d92458670c9ddbc0383e4809a6b9b9dd597e2
Maksthorn <maksdrzezdzon@gmail.com>
2019-10-14 16:28:35 +0100

    register page with some bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,9 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template, url_for
+from forms import RegistrationForm, LoginForm
 import os
 app = Flask(__name__)
 
+app.config['SECRET'] = 'f9779cdd2a5db77c179c4174564e0a5f'
 
 messages = [
                 {'author':'name',
@@ -25,6 +26,16 @@ def hello():
 @app.route("/about")
 def about():
 	return render_template("about.html", title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    login = LoginForm()
+    return render_template('login.html', title='Register', form=form)
 
 if __name__ == "__main__":
     # allows you to dynamically update site like in angular
