95089dbfa160aaf8be7b3363cb5e9885d0010fe8
maksthorn <maksdrzezdzon@gmail.com>
2019-10-15 12:19:47 +0100

    adding models class

--- flaskblog.py
+++ flaskblog.py
@@ -1,37 +1,13 @@
 from flask import Flask,render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from models import User, Post
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f9779cdd2a5db77c179c4174564e0a5f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    # ref method so its capital
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-    
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    # ref table name and field so its lower case
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-    
+db = SQLAlchemy(app)    
 
 messages = [
                 {'author':'name',
