02b91dbe7734fba6e63c1deee78f3d5863f6535f
maksthorn <maksdrzezdzon@gmail.com>
2019-10-15 12:33:59 +0100

    finished project package

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from models import User, Post
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'f9779cdd2a5db77c179c4174564e0a5f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
