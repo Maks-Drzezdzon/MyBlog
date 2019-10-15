b0f2fcb921546ec05da76ae32fc396d7cc459193
maksthorn <maksdrzezdzon@gmail.com>
2019-10-15 14:30:47 +0100

    fixing indentation bug, kill me

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,32 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', 
-		validators=[DataRequired(), Length(min=2,max=20)])
-		# import validators for checking
-	
-	email = StringField('Email',
-		validators=[DataRequired(), Email()])
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('Sign Up')
 
-	password = PasswordField('Password',
-		validators=[DataRequired(), Length(min=8,max=50)])
-    
-	confirm_pass = PasswordField('Confirm Password',
-		validators=[DataRequired(), EqualTo('password')])
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
-	submit_button = SubmitField('Sign up')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
 
 
-class LoginForm(FlaskForm):	
-	email = StringField('Email',
-		validators=[DataRequired(), Email()])
-
-	password = PasswordField('Password', 
-		validators=[DataRequired()])
-	
-	remember = BooleanField('Remember me')
-
-	submit_button = SubmitField('Login')
-    
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
