56be6a2eff74660dc264e18e550b81b0114157c8
maksthorn <maksdrzezdzon@gmail.com>
2019-10-16 16:55:59 +0100

    login, account details and moving register to be under singup/login

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 messages = [
                 {'author':'name',
@@ -28,6 +28,9 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -51,3 +54,14 @@ def login():
         else:
             flash('Oops.. please check user credentials', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+def account():
+    return render_template('account.html', title='Account')
+
