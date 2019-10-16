461256827a0adc6d85090c02724a6c6294fc8d0a
maksthorn <maksdrzezdzon@gmail.com>
2019-10-16 16:37:52 +0100

    adding db func

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
-app = Flask(__name__)
+app = Flask(__name__) # setup for flask
 
 app.config['SECRET_KEY'] = 'f9779cdd2a5db77c179c4174564e0a5f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+db = SQLAlchemy(app) # handles db operations
+bcrypt = Bcrypt(app) # handles pass encrypt
+login_manager = LoginManager(app) # handles sessions
 
 
 from flaskblog import routes
