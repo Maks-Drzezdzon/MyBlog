7ace38c5700b3b7fde23e1bd234adf1839e3b9b2
maksthorn <maksdrzezdzon@gmail.com>
2019-10-29 19:56:56 +0000

    adding form func

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -31,6 +31,23 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired()])
     submit_button = SubmitField('Post')
 
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit_button = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account associated with that email is taken.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit_button = SubmitField('Reset Password')
+
+
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
