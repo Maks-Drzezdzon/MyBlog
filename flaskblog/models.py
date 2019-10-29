0c3000bd866ac2992d883a6afe1169ee3c25fc68
maksthorn <maksdrzezdzon@gmail.com>
2019-10-29 19:48:46 +0000

    setup token defs

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -22,6 +22,16 @@ class User(db.Model, UserMixin):
 
         return s.dumps({'user_id':self.id}).decode('utf-8')
 
+    @staticmethod
+    def_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     
